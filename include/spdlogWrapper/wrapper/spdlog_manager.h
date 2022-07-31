ef1efe3b375e4619c7f63fc3f9d4ef09efa738d0
AsunaU2 <kiritou2@outlook.com>
2022-07-31 21:33:21 +0800

    feat: main modify: 完成json解析函数

--- include/spdlogWrapper/wrapper/spdlog_manager.h
+++ include/spdlogWrapper/wrapper/spdlog_manager.h
@@ -30,10 +30,76 @@ class CJsonConfigTransfer : public IConfigTransfer {
     try {
       std::ifstream ifs{configFilePath_};
       auto data = nlohmann::json::parse(ifs);
-      
+      if (data.contains("config")) {
+        spdlogsink::SinkInfo skInfo;
+        for (const auto &config : data["config"]) {
+          if (config.contains("enable")) {
+            if (!config["enable"]) {
+              continue;
+            }
+
+            // Parse Sink type
+            if (config.contains("sinkType")) {
+              const std::string sinkType = config["sinkType"];
+              if (sinkType == "basic") {
+                skInfo.sink_type = spdlogsink::SinkType::SINK_TYPE_BASIC;
+              } else if (sinkType == "once_file") {
+                skInfo.sink_type = spdlogsink::SinkType::SINK_TYPE_ONCE_FILE;
+              } else if (sinkType == "daily") {
+                skInfo.sink_type = spdlogsink::SinkType::SINK_TYPE_DAILY;
+              } else if (sinkType == "rotating") {
+                skInfo.sink_type = spdlogsink::SinkType::SINK_TYPE_ROTATING;
+              } else if (sinkType == "stdout") {
+                skInfo.sink_type = spdlogsink::SinkType::SINK_TYPE_STDOUT;
+              } else {
+                ret = false;
+              }
+            }
+            // Parse Log path
+            if (config.contains("logPath")) {
+              skInfo.file_path = config["logPath"];
+            }
+            // Parse sinkLevel
+            if (config.contains("sinkLevel")) {
+              const std::string sinkLevel = config["sinkLevel"];
+              if (sinkLevel == "trace") {
+                skInfo.sink_level = spdlogsink::spd_level::trace;
+              } else if (sinkLevel == "debug") {
+                skInfo.sink_level = spdlogsink::spd_level::debug;
+              } else if (sinkLevel == "info") {
+                skInfo.sink_level = spdlogsink::spd_level::info;
+              } else if (sinkLevel == "warning") {
+                skInfo.sink_level = spdlogsink::spd_level::warn;
+              } else if (sinkLevel == "error") {
+                skInfo.sink_level = spdlogsink::spd_level::err;
+              } else if (sinkLevel == "critical") {
+                skInfo.sink_level = spdlogsink::spd_level::critical;
+              } else {
+                ret = false;
+              }
+            }
+            // Parse rotateCount
+            if (config.contains("rotateCount")) {
+              skInfo.rotate_count = config["rotateCount"];
+            }
+            // Parse rotateSize
+            if (config.contains("rotateSize")) {
+              skInfo.rotate_size = config["rotateSize"];
+            }
+            // Parse Pattern
+            if (config.contains("pattern")) {
+              skInfo.sink_pattern = config["pattern"];
+            }
+          }
+          sinkInfos.emplace_back(skInfo);
+        }
+      }
+
     } catch (...) {
       ret = false;
     }
+
+    return {ret, sinkInfos};
   }
 
  private:
--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,38 @@
 
 #include "logger_manager.h"
 #include "sink_manager.h"
+#include "spdlog_manager.h"
 
 using namespace spdlogsink;
 
 int main() {
+  try {
+    std::ifstream ifs{"./config/config.json"};
+    nlohmann::json js = nlohmann::json::parse(ifs);
+    if (js.contains("config")) {
+      auto configArray = js["config"];
+      for (const auto &config : configArray) {
+        if (config.contains("sinkType")) {
+          std::cout << js["sinkType"] << std::endl;
+        }
+        if (js.contains("logPath")) {
+          std::cout << js["logPath"] << std::endl;
+        }
+      }
+    }
+    for (const auto &i : js) {
+      //      if (i["enable"]) {
+      //        std::cout << i["sinkType"] << std::endl;
+      //        std::cout << i["logPath"] << std::endl;
+      //        std::cout << i["rotateSize"] << std::endl;
+      //        std::cout << i["rotateCount"] << std::endl;
+      //        std::cout << i["sinkLevel"] << std::endl;
+      //      }
+    }
+  } catch (...) {
+    std::cerr << "二姐" << std::endl;
+  }
+
   SinkInfo sf;
   std::string pattern = sf.sink_pattern;
   sf.sink_type = SinkType::SINK_TYPE_STDOUT;
