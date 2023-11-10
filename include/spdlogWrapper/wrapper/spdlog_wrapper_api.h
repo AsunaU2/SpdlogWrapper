616b51b110d99cc0bb6303bc7751b3242fccd8ba
AsunaU2 <liyang03@superred.com.cn>
2023-11-10 15:03:32 +0800

    feat: 解决一个warning报告

--- include/spdlogWrapper/wrapper/spdlog_wrapper_api.h
+++ include/spdlogWrapper/wrapper/spdlog_wrapper_api.h
@@ -11,26 +11,26 @@
 #if (defined(_WIN32) || defined(_WIN64))
 #define FILENAME_ (strrchr(__FILE__, '\\') ? (strrchr(__FILE__, '\\') + 1) : __FILE__)
 #else
-#define __FILENAME__ (strrchr(__FILE__, '/') ? (strrchr(__FILE__, '/') + 1) : __FILE__)
+#define FILENAME_ (strrchr(__FILE__, '/') ? (strrchr(__FILE__, '/') + 1) : __FILE__)
 #endif
 
 #define LOG_INIT_(x) CSpdlogManager::GetInstance().Initialize(x)
 
 #define SPDLOGMANAGER_LOG_PRINT CSpdlogManager::GetInstance().LogPrint
 #if (defined(_WIN32) || defined(_WIN64))
-#define LOGT(fmt, ...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::trace, "{}:{} {} " fmt, FILENAME_, __LINE__, __FUNCTION__, __VA_ARGS__)
-#define LOGD(fmt, ...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::debug, "{}:{} {} " fmt, FILENAME_, __LINE__, __FUNCTION__, __VA_ARGS__)
-#define LOGI(fmt, ...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::info, "{}:{} {} " fmt, FILENAME_, __LINE__, __FUNCTION__, __VA_ARGS__)
-#define LOGW(fmt, ...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::warn, "{}:{} {} " fmt, FILENAME_, __LINE__, __FUNCTION__, __VA_ARGS__)
-#define LOGE(fmt, ...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::err, "{}:{} {} " fmt, FILENAME_, __LINE__, __FUNCTION__, __VA_ARGS__)
-#define LOGC(fmt, ...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::critical, "{}:{} {} " fmt, FILENAME_, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define LOGT(fmt, ...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::trace, "[{}:{}] {} " fmt, FILENAME_, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define LOGD(fmt, ...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::debug, "[{}:{}] {} " fmt, FILENAME_, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define LOGI(fmt, ...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::info, "[{}:{}] {} " fmt, FILENAME_, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define LOGW(fmt, ...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::warn, "[{}:{}] {} " fmt, FILENAME_, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define LOGE(fmt, ...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::err, "[{}:{}] {} " fmt, FILENAME_, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define LOGC(fmt, ...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::critical, "[{}:{}] {} " fmt, FILENAME_, __LINE__, __FUNCTION__, __VA_ARGS__)
 #else
-#define LOGT(fmt, args...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::trace, "{}:{} {} " fmt, FILENAME_, __LINE__, __FUNCTION__, ##args)
-#define LOGD(fmt, args...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::debug, "{}:{} {} " fmt, FILENAME_, __LINE__, __FUNCTION__, ##args)
-#define LOGI(fmt, args...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::info, "{}:{} {} " fmt, FILENAME_, __LINE__, __FUNCTION__, ##args)
-#define LOGW(fmt, args...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::warn, "{}:{} {} " fmt, FILENAME_, __LINE__, __FUNCTION__, ##args)
-#define LOGE(fmt, args...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::err, "{}:{} {} " fmt, FILENAME_, __LINE__, __FUNCTION__, ##args)
-#define LOGC(fmt, args...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::critical, "{}:{} {} " fmt, FILENAME_, __LINE__, __FUNCTION__, ##args)
+#define LOGT(fmt, args...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::trace, "[{}:{}] [{}] " fmt, FILENAME_, __LINE__, __FUNCTION__, ##args)
+#define LOGD(fmt, args...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::debug, "[{}:{}] [{}] " fmt, FILENAME_, __LINE__, __FUNCTION__, ##args)
+#define LOGI(fmt, args...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::info, "[{}:{}] [{}] " fmt, FILENAME_, __LINE__, __FUNCTION__, ##args)
+#define LOGW(fmt, args...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::warn, "[{}:{}] [{}] " fmt, FILENAME_, __LINE__, __FUNCTION__, ##args)
+#define LOGE(fmt, args...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::err, "[{}:{}] [{}] " fmt, FILENAME_, __LINE__, __FUNCTION__, ##args)
+#define LOGC(fmt, args...) SPDLOGMANAGER_LOG_PRINT(spdlogsink::spd_level::critical, "[{}:{}] [{}] " fmt, FILENAME_, __LINE__, __FUNCTION__, ##args)
 #endif
 
 #endif  // SPDLOGWRAPPER_INCLUDE_SPDLOGWRAPPER_SPDLOG_API_H_
